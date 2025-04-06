7fe883b11ec37bb2cb410978809a7e64cf2d339a
AytorZ <azumbado80731@ufide.ac.cr>
2025-04-06 13:08:48 -0600

    Main Project Commit
    - Commit inicial con el main project base
    - Plantilla a utilizar y pantalla de index realizadas

---
+++ ColonIA Project/ColonIA Project/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ColonIA Project/ColonIA Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ColonIA_Project.MvcApplication" Language="C#" %>
