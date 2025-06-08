c52b1bc7756149b2db56d2927d6ce44f8f77cb98
Sergio Mauricio Montenegro Pérez <156485337+sergioomp@users.noreply.github.com>
2025-06-08 00:12:42 -0600

    Se configuró un nuevo correo para la recuperación de contraseña. Se agregó una plantilla personalizada para el correo de recuperación de contraseña sobre wwwroot/templates. Se modificó el controlador de recuperación de contraseña para obtener y enviar la plantilla con el código.

--- ColonIA Project/Data/DbColonIaContext.cs
+++ ColonIA Project/Data/DbColonIaContext.cs
@@ -26,7 +26,7 @@ public partial class DbColonIaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:colonaisqldb.database.windows.net,1433;Initial Catalog=DbColonIA;Persist Security Info=False;User ID=AdminSQLcolonAI;Password=MiClaveSegura2025q2ColonAI!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:colondb2.database.windows.net,1433;Initial Catalog=colonIA;Persist Security Info=False;User ID=AdminSQLcolonAI;Password=MiClaveSegura2025q2ColonAI!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
